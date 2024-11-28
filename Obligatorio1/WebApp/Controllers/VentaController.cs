824cdb38c6c91819ea697c9480d2c7c8eaf48ffe
kolobits <camilopardo1994@gmail.com>
2024-11-27 23:11:00 -0300

    Version finalisima

--- Obligatorio1/WebApp/Controllers/VentaController.cs
+++ Obligatorio1/WebApp/Controllers/VentaController.cs
@@ -3,76 +3,83 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApp.Controllers
 {
-    public class VentaController : Controller
-    {
-        Sistema s = Sistema.Instancia();
+	public class VentaController : Controller
+	{
+		Sistema s = Sistema.Instancia();
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        public IActionResult Create(int id)
-        {
-            try
-            {
-                Venta ventaBuscada = s.GetVentaPorId(id);
+		public IActionResult Create(int id)
+		{
+			try
+			{
+				Venta ventaBuscada = s.GetVentaPorId(id);
 
-                int? idLogueado = HttpContext.Session.GetInt32("idLogueado");
-                if (idLogueado == null)
-                {
-                    return RedirectToAction("Login", "Home");
-                }
+				int? idLogueado = HttpContext.Session.GetInt32("idLogueado");
+				if (idLogueado == null)
+				{
+					return RedirectToAction("Login", "Home");
+				}
 
-                Cliente clienteLogueado = s.GetClientePorId(idLogueado.Value);
-                if (clienteLogueado == null)
-                {
-                    return RedirectToAction("Login", "Home");
-                }
-                ViewBag.SaldoDisponible = clienteLogueado.SaldoDisponible;
+				Cliente clienteLogueado = s.GetClientePorId(idLogueado.Value);
+				if (clienteLogueado == null)
+				{
+					return RedirectToAction("Login", "Home");
+				}
+				ViewBag.SaldoDisponible = clienteLogueado.SaldoDisponible;
 
-                return View(ventaBuscada);
-            }
-            catch (Exception e)
-            {
-                ViewBag.MsgError = e.Message;
-                return RedirectToAction("Index", "Home");
-            }
-        }
+				return View(ventaBuscada);
+			}
+			catch (Exception e)
+			{
+				ViewBag.MsgError = e.Message;
+				return RedirectToAction("Index", "Home");
+			}
+		}
 
-        [HttpPost]
-        public IActionResult FinalizarCompra(int id)
-        {
-            try
-            {
-                int? idLogueado = HttpContext.Session.GetInt32("idLogueado");
-                if (idLogueado == null)
-                {
-                    return RedirectToAction("Login", "Home");
-                }
+		[HttpPost]
+		public IActionResult FinalizarCompra(int id)
+		{
+			try
+			{
+				int? idLogueado = HttpContext.Session.GetInt32("idLogueado");
+				if (idLogueado == null)
+				{
+					return RedirectToAction("Login", "Home");
+				}
 
-                Cliente clienteLogueado = s.GetClientePorId(idLogueado.Value);
-                if (clienteLogueado == null)
-                {
-                    return RedirectToAction("Login", "Home");
-                }
+				Cliente clienteLogueado = s.GetClientePorId(idLogueado.Value);
+				if (clienteLogueado == null)
+				{
+					return RedirectToAction("Login", "Home");
+				}
 
 				Publicacion publicacion = s.GetPublicacionPorId(id);
+
+				if (clienteLogueado.SaldoDisponible < publicacion.CalcularPrecioFinal())
+				{
+					ViewBag.MsgError = "No tienes suficiente saldo disponible para realizar esta compra.";
+					ViewBag.SaldoDisponible = clienteLogueado.SaldoDisponible;
+					return View("Create", publicacion);
+				}
 
 				Venta ventaBuscada = (Venta)publicacion;
 
 				publicacion.CerrarPublicacion(clienteLogueado);
 
 				ViewBag.MsgExito = "Compra finalizada exitosamente.";
-                ViewBag.SaldoDisponible = clienteLogueado.SaldoDisponible;
+				ViewBag.SaldoDisponible = clienteLogueado.SaldoDisponible;
 
-                return View("Create", ventaBuscada);
-            }
-            catch (Exception e)
-            {
-                ViewBag.MsgError = e.Message;
-                return RedirectToAction("Create", new { id });
-            }
-        }
-    }
+				return View("Create", ventaBuscada);
+			}
+			catch (Exception e)
+			{
+				ViewBag.MsgError = e.Message;
+				return RedirectToAction("Create", new { id });
+			}
+		}
+	}
 }
